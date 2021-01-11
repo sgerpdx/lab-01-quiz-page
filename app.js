4c1fa01daa7a46d96fae1298d33b17d417a5e5f3
SamuelHGerber <henrylightfoot@gmail.com>
2021-01-11 14:14:48 -0800

    basic html and DOM elments grabbed

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // import functions and grab DOM elements
 
+console.log('hello world');
+
+const startButton = document.getElementById('start-quiz');
+const showResults = document.getElementById('results-box');
+
+console.log(startButton);
+console.log(showResults);
+
 // initialize state
 
 // set event listeners to update state and DOM
