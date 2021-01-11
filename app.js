c07c2ca0073cf927023427394f7da2c61e4b7006
SamuelHGerber <henrylightfoot@gmail.com>
2021-01-11 14:38:39 -0800

    alert function created and validated

--- app.js
+++ app.js
@@ -5,13 +5,14 @@
 const startButton = document.getElementById('start-quiz');
 const showResults = document.getElementById('results-box');
 
-
-
 // initialize state
 
 // set event listeners to update state and DOM
 
 startButton.addEventListener('click', () => {
 
-    console.log('hello world');
+    alert('Strap-in, the Quiz is about to launch!');
+
+
+
 });
