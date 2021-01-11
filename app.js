1a6363aec43a120871a0029811e33d9767070fd1
SamuelHGerber <henrylightfoot@gmail.com>
2021-01-11 14:18:13 -0800

    created and validated event listener

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 // import functions and grab DOM elements
 
-console.log('hello world');
+
 
 const startButton = document.getElementById('start-quiz');
 const showResults = document.getElementById('results-box');
 
-console.log(startButton);
-console.log(showResults);
+
 
 // initialize state
 
 // set event listeners to update state and DOM
+
+startButton.addEventListener('click', () => {
+
+    console.log('hello world');
+});
