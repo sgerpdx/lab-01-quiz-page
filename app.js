e180772de276a4c0ac5bcd8a599756953ea2b681
SamuelHGerber <henrylightfoot@gmail.com>
2021-01-11 17:42:32 -0800

    created checkForNo function and refactored other two questions, flightCheck

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import { checkForYes } from './utils.js';
+
+import { checkForYes, checkForNo } from './utils.js';
 
 
 const startButton = document.getElementById('start-quiz');
@@ -11,17 +12,18 @@ startButton.addEventListener('click', () => {
     alert('Fasten your safety harness, the Quiz is about to launch!');
 
 
+
     const flightCheck = confirm('Are you sure that you will to take the quiz?');
 
-    if (flightCheck === true) {
-        console.log('fly');
-    } else {
+    if (flightCheck !== true) {
 
         return;
+
     }
 
     const firstName = prompt('Please enter your first name');
     const lastName = prompt('Please enter your last name');
+
 
 
     let answersCorrect = 0;
@@ -32,48 +34,27 @@ startButton.addEventListener('click', () => {
     if (checkForYes(answerOne)) {
 
         answersCorrect++;
-        console.log('succeed');
-
-    } else {
-        console.log('fail');
 
     }
 
-
-    // if (answerOne.charAt(0).toUpperCase() === 'Y') {
-    //     console.log('that is right');
-    //     answersCorrect++;
-    //     console.log(answersCorrect);
-
-    // } else {
-    //     console.log('sorry, that is wrong');
-    //     console.log(answersCorrect);
-    // }
 
 
     const answerTwo = prompt('Are the Sun and the Earth always the exact same distance apart?');
 
-    if (answerTwo.charAt(0).toUpperCase() === 'N') {
-        console.log('that is right');
-        answersCorrect++;
-        console.log(answersCorrect);
 
-    } else {
-        console.log('sorry, that is wrong');
-        console.log(answersCorrect);
+    if (checkForNo(answerTwo)) {
+
+        answersCorrect++;
+
     }
+
 
 
     const answerThree = prompt('Is the AU used to measure the heliocentric distances of asteroids?');
 
-    if (answerThree.charAt(0).toUpperCase() === 'Y') {
-        console.log('that is right');
-        answersCorrect++;
-        console.log(answersCorrect);
+    if (checkForYes(answerThree)) {
 
-    } else {
-        console.log('sorry, that is wrong');
-        console.log(answersCorrect);
+        answersCorrect++;
     }
 
 
--- utils.js
+++ utils.js
@@ -2,3 +2,8 @@ export function checkForYes(answer) {
     // this function should take in a user-input string and determine whether the first letter is 'y' (true) or not (false)
     return answer.charAt(0).toUpperCase() === 'Y';
 }
+
+export function checkForNo(answer) {
+    // this function should take in a user-input string and determine whether the first letter is 'n' (true) or not (false)
+    return answer.charAt(0).toUpperCase() === 'N';
+}
